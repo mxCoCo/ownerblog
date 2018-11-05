08108c08eab6fe6d47e87b68c9bb33d3496553d4
mxCoCo <862617618@qq.com>
2018-11-05 18:45:32 +0800

    a

---
+++ ownerblog/src/main/java/com/mx/blog/ownerblog/controller/HelloController.java
@@ -0,0 +1,28 @@
+package com.mx.blog.ownerblog.controller;
+
+import com.mx.blog.ownerblog.domain.BlogUser;
+import com.mx.blog.ownerblog.service.BlogUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/hello")
+public class HelloController {
+    @Autowired
+    private BlogUserService blogUserService;
+
+    @RequestMapping("/test")
+    public String test(Model model){
+        List<BlogUser> blogUserList = blogUserService.findAll();
+        model.addAttribute("size", 1);
+        return "test";
+    }
+    @RequestMapping("/index")
+    public String index(){
+        return "index";
+    }
+}
